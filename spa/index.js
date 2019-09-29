da8f8f2f201bfca11a735ca0ffb8aabc274a1f5b
Priyankagoel <priyanka.goel1997@gmail.com>
2019-09-29 23:35:48 +0530

    assignment1_Priyanka

--- spa/index.js
+++ spa/index.js
@@ -2,23 +2,22 @@ let todos = [
   {
     id: 1,
     name: "Teach Class at Nagarro",
-    done: true
+    done: true,
+    deadline: new Date().toLocaleString()
   },
   {
     id: 2,
     name: "Get Coffee",
-    done: false
+    done: false,
+    deadline: new Date().toLocaleString()
   }
 ];
 
 function render(state) {
   return state
     .map(todo => {
-      // const li = document.createElement('li')
-      // li.classList.add("striked")
-      // document.body.append(li)
       const classString = todo.done ? `class = "list-group-item striked"` : `class = "list-group-item"`
-      return `<li data-todo="${todo.id}" ${classString}> ${todo.name} </li>`;
+      return `<li data-todo="${todo.id}" ${classString}><a href="#" class = "reorder-up"><i class="fa fa-arrow-up" aria-hidden="true"></i></a><a href="#" class = "reorder-down"> <i class="fa fa-arrow-down" aria-hidden="true"></i></a> ${todo.name} <span  class = "deadline"> ${todo.deadline} </span>  </li>`;
     })
     .join("");
 }
@@ -28,12 +27,18 @@ function paint() {
 }
 
 function addTodo() {
-  // document.getElementById('newTodo') != $('#newTodo')
+ 
   const inputBox = $('#newTodo')
+  const inputdate = $('#deadlineDate')
+  const inputtime = $('#deadlineTime')
+  const deadline = new Date(inputdate.val() + ' ' + inputtime.val())
+  const dd = deadline.toLocaleString()
+  
   todos.push({
     id: todos.length + 1,
     name: inputBox.val(),
-    done: false
+    done: false,
+    deadline: dd
   })
 
   inputBox.val('')
@@ -65,3 +70,21 @@ $('#newTodo').on("keypress", function (e) {
 })
 
 paint();
+
+$(".reorder-up").click(function(){
+  var $current = $(this).closest('li')
+  var $previous = $current.prev('li');
+  if($previous.length !== 0){
+    $current.insertBefore($previous);
+  }
+  return false;
+});
+
+$(".reorder-down").click(function(){
+  var $current = $(this).closest('li')
+  var $next = $current.next('li');
+  if($next.length !== 0){
+    $current.insertAfter($next);
+  }
+  return false;
+});
